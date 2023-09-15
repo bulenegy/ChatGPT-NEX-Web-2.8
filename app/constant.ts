b31668dff99b411a788948fb01aeffc7173fec2e
bulenegy <547067717@qq.com>
2023-09-15 17:51:50 +0800

    剔除4.0

--- app/constant.ts
+++ app/constant.ts
@@ -71,30 +71,30 @@ Current time: {{time}}`;
 export const SUMMARIZE_MODEL = "gpt-3.5-turbo";
 
 export const DEFAULT_MODELS = [
-  {
-    name: "gpt-4",
-    available: false,
-  },
-  {
-    name: "gpt-4-0314",
-    available: false,
-  },
-  {
-    name: "gpt-4-0613",
-    available: false,
-  },
-  {
-    name: "gpt-4-32k",
-    available: false,
-  },
-  {
-    name: "gpt-4-32k-0314",
-    available: false,
-  },
-  {
-    name: "gpt-4-32k-0613",
-    available: false,
-  },
+  // {
+  //   name: "gpt-4",
+  //   available: false,
+  // },
+  // {
+  //   name: "gpt-4-0314",
+  //   available: false,
+  // },
+  // {
+  //   name: "gpt-4-0613",
+  //   available: false,
+  // },
+  // {
+  //   name: "gpt-4-32k",
+  //   available: false,
+  // },
+  // {
+  //   name: "gpt-4-32k-0314",
+  //   available: false,
+  // },
+  // {
+  //   name: "gpt-4-32k-0613",
+  //   available: false,
+  // },
   {
     name: "gpt-3.5-turbo",
     available: true,
