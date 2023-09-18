351587d83a216dc5aa186dd93c5edc838050d083
bulenegy <547067717@qq.com>
2023-09-18 19:06:59 +0800

    登录

--- app/components/auth.tsx
+++ app/components/auth.tsx
@@ -1,33 +1,13 @@
 import styles from "./auth.module.scss";
 import { IconButton } from "./button";
-import CloseIcon from "../icons/close.svg";
 import { useNavigate } from "react-router-dom";
-import { Path,HELP_URL } from "../constant";
+import { Path, HELP_URL } from "../constant";
 import { useAccessStore } from "../store";
 import Locale from "../locales";
 
 import BotIcon from "../icons/bot.svg";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { getClientConfig } from "../config/client";
-import { ErrorBoundary } from "./error";
-import {
-  Input,
-  List,
-  ListItem,
-  Modal,
-  PasswordInput,
-  Popover,
-  Select,
-  showConfirm,
-  showToast,
-} from "./ui-lib";
-
-
-
-import ReturnIcon from "../icons/return.svg";
-
-
-
 
 export function AuthPage() {
   const navigate = useNavigate();
@@ -52,32 +32,8 @@ export function AuthPage() {
   }, []);
 
   return (
-    <ErrorBoundary>
-    <div style={{ display: "flex", flexDirection: "column" }}>
-    
-    <div className="window-header" data-tauri-drag-region>
-        <div className="window-header-title">
-          <div className="window-header-main-title">
-            {Locale.Settings.Title}
-          </div>
-          <div className="window-header-sub-title">
-            {Locale.Settings.SubTitle}
-          </div>
-        </div>
-        <div className="window-actions">
-          <div className="window-action-button"></div>
-          <div className="window-action-button"></div>
-          <div className="window-action-button">
-            <IconButton
-              icon={<CloseIcon />}
-              onClick={() => navigate(Path.Home)}
-              bordered
-            />
-          </div>
-        </div>
-      </div>
     <div className={styles["auth-page"]}>
-      
+
       <div className={`no-dark ${styles["auth-logo"]}`}>
         <BotIcon />
       </div>
@@ -115,7 +71,7 @@ export function AuthPage() {
           access.updateToken(e.currentTarget.value);
         }}
       /> */}
-    
+
       <div className={styles["auth-actions"]}>
         <IconButton
           text={Locale.Auth.Confirm}
@@ -133,8 +89,6 @@ export function AuthPage() {
 
       {/* <iframe src="https://j.apagpt.com/help" style={{ width: "100vw", height: "100vh" }} /> */}
     </div>
-    </div>
-    
-    </ErrorBoundary>
+
   );
 }
--- app/components/login.tsx
+++ app/components/login.tsx
@@ -23,7 +23,7 @@ export function LoginPage() {
   const access = useAccessStore();
   const accessStore = useAccessStore();
 
-  
+
   const [showIframe, setShowIframe] = useState(false);
   const [buttonText, setButtonText] = useState("立即购买");
 
@@ -41,75 +41,69 @@ export function LoginPage() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <ErrorBoundary>
-      <div className={styles["mask-page"]}>
-        <div className="window-header">
-          <div className="window-header-title">
-            <div className="window-header-main-title">
-              登录
-              {/* {Locale.Mask.Page.Title} */}
-            </div>
-            <div className="window-header-submai-title">
-              开始使用ChatGPT
-              {/* {Locale.Mask.Page.SubTitle(allMasks.length)} */}
-            </div>
+    <div className={styles["mask-page"]}>
+      <div className="window-header">
+        <div className="window-header-title">
+          <div className="window-header-main-title">
+            登录
+            {/* {Locale.Mask.Page.Title} */}
           </div>
-
-          <div className="window-actions">
-            <div className="window-action-button">
-              <IconButton
-                icon={<CloseIcon />}
-                bordered
-                onClick={() => navigate(Path.Home)}
-              />
-            </div>
+          <div className="window-header-submai-title">
+            开始使用ChatGPT
+            {/* {Locale.Mask.Page.SubTitle(allMasks.length)} */}
           </div>
         </div>
-        {/* <div className={styles2["auth-page"]}>
-        <div style={{ padding: '1rem', overflow: 'scroll' }}>
-          <div className="LoginConten"> */}
-          <div className={styles2["auth-page"]}>
-            <div className={`no-dark ${styles2["auth-logo"]}`}>
-                <BotIcon />
-            </div>
-
-            <div className={styles2["auth-title"]}>{Locale.Auth.Title}</div>
-            <div className={styles2["auth-tips"]}>{Locale.Auth.Tips}</div>
-
-
-            <input
-                className={styles2["auth-input"]}
-                type="text"
-                placeholder={Locale.Auth.Input}
-                value={accessStore.token}
-                onChange={(e) => {
-                access.updateToken(e.currentTarget.value);
-                }}
+        <div className="window-actions">
+          <div className="window-action-button">
+            <IconButton
+              icon={<CloseIcon />}
+              bordered
+              onClick={() => navigate(Path.Home)}
             />
-            
-            <div className={styles2["auth-actions"]}>
-                <IconButton
-                text={Locale.Auth.Confirm}
-                type="primary"
-                onClick={goHome}
-                />
-                {/* 返回按钮 */}
-                {/* <IconButton text={Locale.Auth.Later} onClick={goHome} /> */}   
-                {/* <IconButton text="立即购买" onClick={() => window.open(HELP_URL, '_blank')} /> */}
-                <IconButton text={buttonText} onClick={handleBuyNow} />
-            </div>
-
-            {showIframe && (
-                <iframe src="https://j.apagpt.com/help" style={{ width: "100%", height: "100%" }} />
-            )}
-
-            {/* <iframe src="https://j.apagpt.com/help" style={{ width: "100vw", height: "100vh" }} /> */}
-            </div>
           </div>
+        </div>
+      </div>
+      <div className={styles2["auth-page"]}>
+        <div className={`no-dark ${styles2["auth-logo"]}`}>
+          <BotIcon />
+        </div>
+
+        <div className={styles2["auth-title"]}>{Locale.Auth.Title}</div>
+        <div className={styles2["auth-tips"]}>{Locale.Auth.Tips}</div>
+
+
+        <input
+          className={styles2["auth-input"]}
+          type="text"
+          placeholder={Locale.Auth.Input}
+          value={accessStore.token}
+          onChange={(e) => {
+            access.updateToken(e.currentTarget.value);
+          }}
+        />
+
+        <div className={styles2["auth-actions"]}>
+          <IconButton
+            text={Locale.Auth.Confirm}
+            type="primary"
+            onClick={goHome}
+          />
+          {/* 返回按钮 */}
+          {/* <IconButton text={Locale.Auth.Later} onClick={goHome} /> */}
+          {/* <IconButton text="立即购买" onClick={() => window.open(HELP_URL, '_blank')} /> */}
+          <IconButton text={buttonText} onClick={handleBuyNow} />
+        </div>
+
+        {showIframe && (
+          <iframe src="https://j.apagpt.com/help" style={{ width: "100%", height: "100%" }} />
+        )}
+
+        {/* <iframe src="https://j.apagpt.com/help" style={{ width: "100vw", height: "100vh" }} /> */}
+      </div>
+    </div>
 
         {/* </div>
         </div>
       </div> */}
-    </ErrorBoundary>
   );
 }
