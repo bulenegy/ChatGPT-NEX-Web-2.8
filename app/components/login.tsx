5201ac885d7c3e7e6c52c88c54bf515c625593b4
bulenegy <547067717@qq.com>
2023-09-18 19:27:16 +0800

    Update login.tsx

--- app/components/login.tsx
+++ app/components/login.tsx
@@ -41,29 +41,8 @@ export function LoginPage() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <div>
-      <div className="window-header">
-        <div className="window-header-title">
-          <div className="window-header-main-title">
-            登录
-            {/* {Locale.Mask.Page.Title} */}
-          </div>
-          <div className="window-header-submai-title">
-            开始使用ChatGPT
-            {/* {Locale.Mask.Page.SubTitle(allMasks.length)} */}
-          </div>
-        </div>
-        <div className="window-actions">
-          <div className="window-action-button">
-            <IconButton
-              icon={<CloseIcon />}
-              bordered
-              onClick={() => navigate(Path.Home)}
-            />
-          </div>
-        </div>
-      </div>
-          <iframe src="https://j.apagpt.com/help" style={{ width: "100%", height: "100%" }} />
-    </div>
+
+      <iframe src="https://j.apagpt.com/help" style={{ width: "100%", height: "100%" }} />
+
   );
 }
