e8124f3808b85a4300748805875c99bf344ff9d1
bulenegy <547067717@qq.com>
2023-09-18 19:11:39 +0800

    Update login.tsx

--- app/components/login.tsx
+++ app/components/login.tsx
@@ -41,7 +41,7 @@ export function LoginPage() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <div className={styles["mask-page"]}>
+    <div className={styles2["auth-page"]}>
       <div className="window-header">
         <div className="window-header-title">
           <div className="window-header-main-title">
