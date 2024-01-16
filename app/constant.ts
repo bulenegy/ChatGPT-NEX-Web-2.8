9b3e99ba5e937a8bcfcfb2334a7a9ff6aa38f86f
bulenegy <547067717@qq.com>
2024-01-16 15:29:52 +0800

    mj

--- app/constant.ts
+++ app/constant.ts
@@ -119,6 +119,13 @@ export const DEFAULT_MODELS = [
     name: "gpt-3.5-turbo-16k-0613",
     available: true,
   },
+  //a5470
+  //midjourney功能start
+  {
+    name: "midjourney",
+    available: true,
+  },
+  //midjourney功能over
 ] as const;
 
 export const CHAT_PAGE_SIZE = 15;
